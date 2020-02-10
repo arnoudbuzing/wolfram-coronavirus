88c8c0443c21231a5bfad276e48bdfd8b189ad2a
Arnoud Buzing <arnoudbuzing@users.noreply.github.com>
2020-02-10 14:19:09 -0600

    updates

---
+++ CoronavirusTools/Kernel/CoronavirusTools.m
@@ -0,0 +1,11 @@
+BeginPackage["CoronavirusTools`"];
+
+test:usage = "testing 123"
+
+Begin["`Private`"];
+
+test[x_] := x^2;
+
+End[]; (* `Private` *)
+
+EndPackage[]
---
+++ CoronavirusTools/PacletInfo.m
@@ -0,0 +1,18 @@
+(* Paclet Info File *)
+(* Created by the Wolfram Language Plugin for IntelliJ, see http://wlplugin.halirutan.de/ *)
+(* :Author: arnoudb *)
+(* :Date: 2/10/2020 *)
+
+Paclet[
+  Name -> "CoronavirusTools",
+  Version -> "0.1",
+  MathematicaVersion -> "12.0",
+  Description -> "Tools for doing data analysis on the coronavirus",
+  Creator -> "Arnoud Buzing",
+  Extensions ->
+      {
+        {"Kernel", Root -> "Kernel", Context -> "CoronavirusTools`"}
+      }
+]
+
+
